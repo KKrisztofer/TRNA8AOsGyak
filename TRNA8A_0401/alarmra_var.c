c786ca1345d9f41ee182ae3faafc0537a69fa9f3
KKrisztofer <79247395+KKrisztofer@users.noreply.github.com>
2021-04-27 23:08:28 +0200

    TRNA8A_0401

---
+++ TRNA8A_0401/alarmra_var.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void semmi();
+
+int main(){
+    unsigned sec = 5;
+    signal(SIGALRM, semmi);
+    alarm(sec);
+
+    printf("%d varok.\n",1);
+    pause();
+
+    printf("Megerkezett az alarm.\n");
+
+	return 0;
+}
+
+void semmi(){ ; }
